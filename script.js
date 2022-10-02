325a80b6851502e3b6fb4ba7fc9568733e3da7eb
Carlos Iego <carlosiego2017@gmail.com>
2022-10-02 08:56:47 -0300

    mudanças

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-function entrada(n){
+const valores = document.querySelectorAll('.botao')
+valores.forEach((valor) => {
+    valor.addEventListener('click', () => {
+        if (valor.value == 'CE'){
+            limpar()
+        }else if(valor.value == '='){
+            calcular()
+        }
+        else{
+            entrada(valor)
+        }
+    }
+)})
+
+
+function entrada(valor){
     var res = document.getElementById('res')
     if(res.innerHTML.length <= 21){
-        res.innerHTML += `${n}`
+        res.innerHTML += `${valor.value}`
     }
     var resprev = document.getElementById('res').innerHTML
     if (resprev.length >= 3){
@@ -12,19 +27,14 @@ function entrada(n){
 function limpar(){
     res.innerHTML = ''
     document.getElementById('prev').innerHTML = ''
-}
+    }    
 
 function calcular(){
     var resultado = document.getElementById('res').innerHTML
-    document.getElementById('prev').innerHTML = '   '
+    document.getElementById('prev').innerHTML = ''
     if (resultado){
         document.getElementById('res').innerHTML = eval(resultado)
     }else{
         document.getElementById('res').innerHTML = 'Error'
     }
-}
-
-function color(cor){
-    var backg = document.getElementById('principal')
-    backg.style.backgroundImage = `${cor}`
 }
